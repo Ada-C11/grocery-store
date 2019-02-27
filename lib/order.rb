1688e1bb7bef6efcb00f6d7c08c5bc6713a854fa
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-02-27 14:56:45 -0800

    added Order class with constructor

---
+++ lib/order.rb
@@ -0,0 +1,16 @@
+class Order
+  attr_reader :id, :products, :customer, :fulfillment_status
+
+  def initialize(id, products, customer, fulfillment_status = :pending)
+    @id = id
+    @products = products
+    @customer = customer
+    @fulfillment_status = fulfillment_status
+  end
+
+  def total
+  end
+
+  def add_product
+  end
+end
