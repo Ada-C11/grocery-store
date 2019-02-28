8e6bf607af280de24be0f6548eaba1ae4e3b4bf3
Tatiana Quintana <tatsqui@gmail.com>
2019-02-27 17:29:20 -0800

    "initialize Order passing first round of tests"

--- lib/order.rb
+++ lib/order.rb
@@ -3,51 +3,22 @@ require 'Rake'
 require 'awesome_print'
 
 class Order
-  attr_reader :order_id
-  attr_accessor :products, :fulfillment_status
+  attr_reader :id
+  attr_accessor :products, :fulfillment_status, :customer, :products
 
-  def initialize order_id
-    @order_id = order_id
-    @products = {}
-  end
+  def initialize id, products, customer, fulfillment_status = :pending
+    @id = id
+    @fulfillment_status = fulfillment_status
+    @customer = customer
+    @products = products
 
-  def products(given_hash) 
-    @products = given_hash
-  end
-
-  def customer=(customer_info)
-    @customer = customer_info
-  end
-  
-  def fulfillment_status(status = :pending)
-    if status == nil || status.class == Integer
+    unless [:pending, :paid, :complete, :processing, :shipped].include?(fulfillment_status)
       raise ArgumentError, "This is an invalid status"
     end
-
-    case status
-    when :pending
-      @fulfillment_status = "Pending"
-    when :paid
-      @fulfillment_status = :paid
-    when :processing
-      @fulfillment_status = :processing
-    when :shipped
-      @fulfillment_status = :shipped
-    when :complete
-      @fulfillment_status = :complete
-    when ""
-      raise ArgumentError, "This is an invalid status"
-    end
-    if status == nil
-    raise ArgumentError, "This is an invalid status"
-    end
-    return status
   end
 
   def total
-    # sums up the products
-    # includes 7.5% sales tax
-    # rounds the result to two decimal places
+    
   end
 
   def add_product #(has two parameters)
@@ -56,7 +27,3 @@ class Order
 
 end
 
-
-tatiana = Order.new(1003)
-puts tatiana.order_id
-puts tatiana.fulfillment_status(:complete)
