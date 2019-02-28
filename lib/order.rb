8af79c74ccf10ff3f8743e7e67027f9c1c421f51
Amy Phung <phung.amy85@gmail.com>
2019-02-27 17:12:54 -0800

    Passes Wave 1 tests

--- lib/order.rb
+++ lib/order.rb
@@ -8,31 +8,27 @@ def initialize(id, products, customer, fulfillment_status = :pending)
   @id = id
   @products = products
   @customer = customer
-  @fulfillment_status = fulfillment_status
 
-  case status
-  when @fulfillment_status != :pending, :paid, :processing, :shipped, :complete
+  case fulfillment_status
+  when :pending, :paid, :processing, :shipped, :complete
+    @fulfillment_status = fulfillment_status
+  else
     raise ArgumentError, "Please enter a valid fulfillment status"
   end
-
 end
 
 def total
-  ((@products.values.sum) * 1.075).round(2)
+ total = ((@products.values.sum) * 1.075).round(2)
 return total
 end
 
 def add_product(product_name, price)
-  @products[product_name] = price
 
-  if @products.include?(product_name)
-    raise ArgumentError, "That item has already been added"
+  @products.key?(product_name) ? (raise ArgumentError, "That item has already been added") : @products.store(product_name, price)
+  # @products.store(product_name, price)
+
+  # if @products.key?(product_name)
+  #   raise ArgumentError, "That item has already been added"
   end
-  return @products
 end
 
-
-add_product("apple" => 2.2)
-puts products
-shop = Order.new(22, products, "amy", :pending)
-puts shop
