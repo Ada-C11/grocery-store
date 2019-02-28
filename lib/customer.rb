b940e0cb7e787ecb1646ff55abeef28c70c77d90
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-02-28 15:27:04 -0800

    added class method that returns an array of all customer instances

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,7 +1,11 @@
+# require "awesome_print"
+# require_relative '../data/csv_practice'
+require "csv"
+
 # class that creates an instance of a customer
 class Customer
 
-  # creates methods that will read or read/write certain instance variables
+  # creates methods that will allow reading or reading/writing of specified instance variables
   attr_reader :id
   attr_accessor :email, :address
 
@@ -20,5 +24,30 @@ class Customer
       address: @address,
     }
     return customer_hash
+  end
+
+  # method that returns an array of all customer instances using data pulled from the customers.csv file
+  def self.all
+    all_customers = []
+
+    CSV.open("data/customers.csv", "r").each do |line|
+      address = {}
+      address[:steet] = line[2]
+      address[:city] = line[3]
+      address[:state] = line[4]
+      address[:zip] = line[5]
+
+      all_customers << Customer.new(line[0], line[1], address)
+    end
+    return all_customers
+  end
+
+  # returns an instance of Customer where the value of the id field in the CSV matches the passed parameter
+  def self.find(id)
+    Customer.all.each do |customer|
+      if customer.id == id
+        return customer
+      end
+    end
   end
 end
