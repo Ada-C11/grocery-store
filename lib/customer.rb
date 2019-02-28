f0ee661de77c20072cc8ee7750c8ccd29773946a
qqdipps <sdipp007@gmail.com>
2019-02-27 16:28:19 -0800

    added method add product, total

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,7 +1,28 @@
+require "csv"
+
 class Customer
   attr_reader :id
-  attr_accesoor :email, :delivery_address
+  attr_accessor :email, :address
 
-  def initialize
+  def initialize(id, email, address)
+    @id = id
+    @email = email
+    @address = address
+  end
+
+  def self.all
+    customers_all = []
+    CSV.open("../data/customers.csv", "r").each do |line|
+      id = line[0]
+      email = line[1]
+      address = {
+        street: line[3],
+        city: line[4],
+        state: line[5],
+        zip: line[6],
+      }
+      customers_all << self.new(id, email, address)
+    end
+    return customers_all
   end
 end
--- lib/order.rb
+++ lib/order.rb
@@ -1,13 +1,32 @@
-require_relative "customer"
+
 
 class Order
   attr_reader :id
-  attr_accessor :collection, :customer, :fulfillment_status
+  attr_accessor :products, :customer, :fulfillment_status
 
-  def initialize(fulfillment_status: defualt)
+  def initialize(id, products, customer, fulfillment_status = :pending)
     if ![:pending, :paid, :processing, :shipped, :complete].include?(fulfillment_status)
       raise ArgumentError.new("#{fulfillment_status} not a valid fullfillment status")
     end
     @fulfillment_status = fulfillment_status
+    @products = products
+    @customer = customer
+    @id = id
+  end
+
+  def total
+    total_cost = products.sum do |name, cost|
+      cost *= 1.075
+    end
+    return total_cost.round(2)
+  end
+
+  # An add_product method which will take in two parameters, product name
+  # and price, and add the data to the product collection
+  # If a product with the same name has already been added to the order, an ArgumentError should be raised
+
+  def add_product(name, price)
+    raise ArgumentError.new("#{name}\'s name is in use. ") if products[name]
+    products[name] = price
   end
 end
