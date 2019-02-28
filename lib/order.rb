c53dc68fcf28c03e9c38e950221ed7f5555aa938
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-02-27 16:16:21 -0800

    added total method to order class

--- lib/order.rb
+++ lib/order.rb
@@ -6,9 +6,19 @@ class Order
     @products = products
     @customer = customer
     @fulfillment_status = fulfillment_status
+    if (@fulfillment_status != :pending && @fulfillment_status != :shipped && @fulfillment_status != :paid && @fulfillment_status != :processing && @fulfillment_status != :complete)
+      raise ArgumentError, "That is not a valid fulfillment status."
+    end
   end
 
   def total
+    sum = 0
+    @products.each_value do |price|
+      sum += price
+      puts sum
+    end
+    sum_with_tax = sum + (sum * 0.075)
+    return sum_with_tax.round(2)
   end
 
   def add_product
