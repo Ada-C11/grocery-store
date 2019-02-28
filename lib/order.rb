8643211fd6c2f75a95df593a145df29d6a669478
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-02-27 17:02:27 -0800

    added comments and refactored order class

--- lib/order.rb
+++ lib/order.rb
@@ -1,17 +1,23 @@
+# class that creates an instance of an order
 class Order
+
+  # creates methods that will allow reading or writing of specified instance variables
   attr_reader :id, :products, :customer, :fulfillment_status
   attr_writer :fulfillment_status
 
+  # constructor that sets up each instance of an order's id, products, customer, and fulfillment
+  # status. Raises an ArgumentError if fulfillment entered is not a valid option
   def initialize(id, products, customer, fulfillment_status = :pending)
     @id = id
     @products = products
     @customer = customer
     @fulfillment_status = fulfillment_status
-    if @fulfillment_status != :pending && @fulfillment_status != :paid && @fulfillment_status != :processing && @fulfillment_status != :shipped && @fulfillment_status != :complete
+    unless [:pending, :paid, :processing, :shipped, :complete].include?(@fulfillment_status)
       raise ArgumentError, "Fulfillment status must be :pending, :paid, :processing, :shipped, :complete!"
     end
   end
 
+  # method that returns a hash containing the id, products, customer, and fulfillment status for an order
   def new_order
     @order_hash = {
       id: @id,
@@ -22,14 +28,12 @@ class Order
     return @order_hash
   end
 
+  # method that returns the total value, plus 7.5% tax of all products in an order
   def total
-    total_value = 0
-    @products.each do |product, value|
-      total_value += value
-    end
-    return ("%.2f" % (total_value * 1.075)).to_f
+    return @products.sum { |product, value| ("%.2f" % (value * 1.075)).to_f }
   end
 
+  # method that adds a product and price to the existing products hash of an order
   def add_product(product_name, price)
     if @products.keys.include?(product_name)
       raise ArgumentError, "A product with this name has already been added to the order!"
@@ -37,15 +41,4 @@ class Order
 
     @products[product_name] = price
   end
-
-  # id (number)
-
-  # collection of products & their costs (hash)
-  # # an empty hash is OK
-  # # can assume there is only one of each product
-
-  # an instance of a customer (person who placed the order)
-
-  # fulfillment status (a symbol, :pending, :paid, :processing, :shipped, :complete)
-  # # should default to :pending, if something else is given must raise an Argument Error!
 end
