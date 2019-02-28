5757e23987b85f3ddd9701ec7942f7295f84c097
Nidhi <nidhixpatel@gmail.com>
2019-02-28 14:40:05 -0800

    completed wave 2

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Customer
 
     attr_reader :id
@@ -11,9 +13,19 @@ class Customer
     end
 
     def self.all
+        customer_array = []
+        CSV.read("data/customers.csv", headers: true).each do |line|
+            full_address = "#{line["address1"]}, #{line["city"]},  #{line["zipcode"]}"
+            new_customer = self.new(line["id"].to_i, line["email"], full_address)
+            customer_array.push(new_customer)
+        end
+        return customer_array
     end
 
     def self.find(id)
+        customers = self.all
+        found_customer = (customers.select{|customer| id == customer.id})[0]
+        return found_customer
     end
 
 end
