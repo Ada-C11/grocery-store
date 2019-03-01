faf514c7fcd0074d33029601dde4c41954984322
Kate Sandler <kate@katekatekate.net>
2019-03-01 04:49:02 -0800

    includes working find method

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,6 +1,6 @@
 require 'csv'
 require 'awesome_print'
-
+# Customer contains init, processes a CSV into an array, and has a find method
 class Customer
   attr_accessor(:email, :address, :id)
   # Contains methods pertaining to customers
@@ -10,19 +10,20 @@ class Customer
     @id = id
   end
 
+  # reads CSV of customer data into array of arrays
   def self.all
     customers = []
-    CSV.foreach('data/customers.csv') do |row|
-      new_customer = Customer.new(row[0].to_i, row[1], %i[street city state zip].zip(row.slice(2..5)).to_h)
+    CSV.foreach('data/customers.csv') do |r|
+      new_customer = Customer.new(r[0].to_i, r[1], %i[street city state zip].zip(r.slice(2..5)).to_h)
       customers << new_customer
     end
     customers
   end
-  # returns a collection of `Customer` instances, representing all of the Customer described in the CSV fil
 
-  def self.find
-    # uses Customer.all(id)
-    # returns an instance of `Customer` where the value of the id field in the CSV matches the passed parameter
-    # raise ArgumentError if ID does not exist
+  # finds and returns customer with passed id (or nil if no record has passed id)
+  def self.find(id)
+    all_customers = Customer.all
+    customer = all_customers.detect { |record| record.id == id }
+    customer
   end
 end
