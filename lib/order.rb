9cb8affbbbf227e2727857ca00df7361484ef945
Mina Shin <minamishin@hotmail.com>
2019-03-01 14:45:40 -0800

    Order all and find complete

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,6 @@
+require 'csv'
+require 'awesome_print'
+
 class Order
     attr_reader :id, :products, :customer, :fulfillment_status
 
@@ -29,5 +32,24 @@ class Order
             raise ArgumentError, "Item is already in inventory."
         end
         @products[product_name] = [price]
+    end
+
+    # open CSV and put into an array each line of the CSV
+    def self.all
+        new_order = []
+        CSV.open('data/orders.csv', 'r').each do |line|
+            new_order << Order.new(line[0], line[1], line[2], line[3])
+        end
+        return new_order
+    end
+
+    # pull all the order information, look for ID, match?
+    def self.find(id)
+        Order.all.each do |order_number|
+            if order_number.id == id
+                return order_number
+            end 
+        end
+        return nil
     end
 end
