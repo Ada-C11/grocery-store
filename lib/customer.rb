4b58e3b2ce5a2ace555d89d27af6d222074ff810
amyesh <amyesh08@gmail.com>
2019-02-28 16:48:13 -0800

    Wave 2 Customer.all method

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 class Customer
   attr_reader :id
   attr_accessor :email, :address
@@ -6,5 +8,21 @@ class Customer
     @id = id
     @email = email
     @address = address
+  end
+
+  def self.all
+    customer_info = []
+    CSV.open("data/customers.csv", "r").each do |customer|
+      id = customer[0].to_i
+      email = customer[1].to_s
+      address = {
+        address: customer[2].to_s,
+        city: customer[3].to_s,
+        state: customer[4].to_s,
+        zipcode: customer[5].to_s,
+      }
+      customer_info << Customer.new(id, email, address)
+    end
+    return customer_info
   end
 end
