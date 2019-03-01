5ffb4bb54dacbd42b664eda3ba53b93e0c701397
Jessica <jessica.homet@gmail.com>
2019-02-28 23:09:13 -0800

    fixed self.find method to use self.all method instead of parsing CSV file

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,8 +1,8 @@
 require "csv"
+require "awesome_print"
 
 class Customer
-  attr_reader :id
-  attr_accessor :email, :address
+  attr_accessor :email, :address, :id
 
   def initialize(id, email, address)
     @id = id.to_i
@@ -25,9 +25,9 @@ class Customer
 
   # returns an instance of `Customer` where the value of the id field in the CSV matches the passed parameter
   def self.find(id)
-    CSV.open("data/customers.csv", "r").each do |line|
-      if line[0].to_i == id
-        return Customer.new(line[0].to_i, line[1], line[2])
+    Customer.all.each do |customer|
+      if customer.id == id
+        return customer
       end
     end
     return nil
