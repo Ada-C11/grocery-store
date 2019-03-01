f57256c4ca030464ae5f5a0fe2bc7377c8ea4797
Angela <angela.ohh@gmail.com>
2019-02-28 18:26:01 -0800

    added self.find class method

--- lib/customer.rb
+++ lib/customer.rb
@@ -16,13 +16,32 @@ class Customer
       # change ID into integer, not string
       info[0] = info[0].to_i
 
+      # customer_info_instance is an array, change to hash
       customer_info_instance = Customer.new(info[0], info[1], info[2])
+      # customer_info_hash = {}
+      # customer_info_hash["id"] = customer_info_instance[0].id
+      # customer_info_hash["email"] = customer_info_instance[1].email
+      # customer_info_hash["address"] = customer_info_instance[2].address
+
+      # all_customer_info << customer_info_hash
       all_customer_info << customer_info_instance
     end
 
     return all_customer_info
   end
 
-  #   def find(id)
-  #   end
+  def self.find(id)
+    # # one array of all Customer instances. Each index holds one instance.
+    all_info = Customer.all
+
+    # # iterate through each customer
+    all_info.each do |customer|
+      if customer.id == id
+        return customer
+      end
+    end
+    return nil
+  end
 end
+
+# practice = Customer.new(35, "angela@google.com", "134 flower lane")
