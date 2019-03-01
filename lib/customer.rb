f95a16382ab9c243b6d3a4e5e7a670b921e76061
Tatiana Quintana <tatsqui@gmail.com>
2019-02-28 16:16:39 -0800

    "customer wave 2 tests almost all passed"

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,5 +1,6 @@
 require 'awesome_print'
 require 'csv'
+require 'pry'
 
 class Customer
   attr_reader :id
@@ -15,14 +16,25 @@ class Customer
     #return array of all customers
     all_customers = []
     CSV.open('data/customers.csv', 'r').each do |line|
-      id = line[0], email = line[1], address = { street: line[2], city: line[3], state: line[4], zip: line[5] }
+      id = line[0].to_i 
+      email = line[1] 
+      address = { street: line[2], city: line[3], state: line[4], zip: line[5] }
       customer = Customer.new(id, email, address)
       all_customers << customer
     end
     return all_customers
   end
 
-  # def self.find
-  # end
+  def self.find(search)
+    not_found = nil
+    Customer.all.each do |customer|
+      binding.pry
+      if customer.id == search
+        return customer
+      elsif customer.id = nil
+        return not_found
+      end
+    end
+  end
 end
 
