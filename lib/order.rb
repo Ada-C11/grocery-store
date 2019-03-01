e0a3505a5527255b458dcb7af7ab3145cd5e9d37
Shubha Rajan <shubharajan94@gmail.com>
2019-02-28 20:07:54 -0800

    passes tests! changed args for order initialize method to use optional positional arg instead of kwarg

--- lib/order.rb
+++ lib/order.rb
@@ -1,18 +1,18 @@
 class Order
   attr_reader :id
+  attr_accessor :customer, :fulfillment_status, :products
 
-  def initialize(id, products, customer, fulfillment_status: :pending)
+  def initialize(id, products, customer, fulfillment_status = :pending)
     valid_status = [:pending, :paid, :processing, :shipped, :complete]
     if !(valid_status.include?(fulfillment_status))
       raise ArgumentError, "Invalid fulfillment_status value. Acceptable " +
                            "values include :pending, :paid, :processing," +
                            " :shipped, or :complete."
-    else
-      @id = id
-      @products = products
-      @customer = customer
-      @fulfillment = fulfillment_status
     end
+    @id = id
+    @products = products
+    @customer = customer
+    @fulfillment_status = fulfillment_status
   end
 
   def total
@@ -21,11 +21,17 @@ class Order
   end
 
   def add_product(name, price)
-    if products.keys.include?(name.downcase)
+    if @products.keys.include?(name.downcase)
       raise ArgumentError, "A product with the same name has already " +
                            "been added to the order."
-    else
-      @products[name] = price.to_f.round(2)
     end
+    @products[name.downcase] = price.to_f.round(2)
+  end
+
+  def remove_product(name, price)
+    if !(@products.keys.include?(name.downcase))
+      raise ArgumentError, "Order # #{@id} does not include the product #{name}. "
+    end
+    @products.delete(name.downcase)
   end
 end
