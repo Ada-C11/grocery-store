de26653e72a49e0692a5917a0b9d62bcf13a67f9
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-03-01 15:12:09 -0800

    Adds self.find method to Order and passes Wave 2 tests

--- lib/order.rb
+++ lib/order.rb
@@ -1,48 +1,25 @@
+require 'csv'
+require 'awesome_print'
+require_relative 'customer'
+
+ORDER_FILEPATH = 'data/orders.csv'
 # WAVE 2
-
-# Order
-# Create a class called Order. Each new Order should include the following attributes:
-
-# ID, a number (read-only)
-# A collection of products and their cost. This data will be given as a hash that looks like this:
-# { "banana" => 1.99, "cracker" => 3.00 }
-# Zero products is permitted (an empty hash)
-# You can assume that there is only one of each product
-# An instance of Customer, the person who placed this order
-# A fulfillment_status, a symbol, one of :pending, :paid, :processing, :shipped, or :complete
-# If no fulfillment_status is provided, it will default to :pending
-# If a status is given that is not one of the above, an ArgumentError should be raised
-# In addition, Order should have:
-
-# A total method which will calculate the total cost of the order by:
-# Summing up the products
-# Adding a 7.5% tax
-# Rounding the result to two decimal places
-# An add_product method which will take in two parameters, product name and price, and add the data to the product collection
-# If a product with the same name has already been added to the order, an ArgumentError should be raised
-# Optional:
-# Make sure to write tests for any optionals you implement!
-
-# Add a remove_product method to the Order class which will take in one parameter, a product name, and remove the product from the collection
-# If no product with that name was found, an ArgumentError should be raised
-
 class Order
   
   attr_reader :id
   attr_accessor :products, :customer, :fulfillment_status
   # Constructor
   def initialize(id, products, customer, fulfillment_status = :pending)
-    
-    # raises error if the below fulfillment_statuses are not provided as an argument
+    # THIS WORKED BEFORE THE SELF.LOAD AND SELF.ALL METHODS WERE CREATED!
     if (fulfillment_status != :pending) && (fulfillment_status != :paid) && (fulfillment_status != :processing) && (fulfillment_status != :shipped) && (fulfillment_status != :complete)
       raise ArgumentError.new("This is not a real status")
     end
-    # uses :pending as default fulfillment status if no status is provided
+   
     if fulfillment_status == nil
       fulfillment_status = :pending
     end
 
-    @id = id
+    @id = id.to_i
     @products = products
     @customer = customer
     @fulfillment_status = fulfillment_status
@@ -69,8 +46,52 @@ class Order
     if @products.include?(name)
       raise ArgumentError.new("A product with this name already exists")
     end
-
     @products[name] = price
-
   end
-end # class Order end
+
+  def self.load(filepath)
+    orders = Array.new
+
+    puts filepath
+
+    CSV.open(filepath, 'r').each do |csv_row|
+      id = csv_row[0]
+      products_arr = csv_row[1].split(";")
+      products_hash = products_arr.map { |i| i.split ':'}.to_h
+      products = products_hash.map { |k, v| [k.to_s, v.to_f] }.to_h
+      customer = Customer.find(csv_row[2].to_i)
+      fulfillment_status = csv_row[3]
+
+      order = Order.new(id, products, customer, fulfillment_status.to_sym)
+      orders.push(order)
+    end 
+    return orders
+  end
+
+  def self.all
+    orders_data = Order.load(ORDER_FILEPATH)
+    return orders_data
+  end
+
+  # returns an instance of Order where the value of the id field in the CSV matches the passed parameter
+  def self.find(id)
+    found_order = nil
+    Order.all.each do |find_order|
+      if find_order.id == id
+        found_order = find_order
+      end
+    end
+    return found_order
+  end
+
+
+end # class Order
+
+puts "All orders:"
+ap Order.all
+puts "First order:"
+ap Order.all.first
+puts "Last order:"
+ap Order.all.last
+puts "Find order:"
+puts Order.find(1)
