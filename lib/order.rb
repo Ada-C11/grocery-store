9d4621345ee277d73c9ab467d93baf5a946b72cc
Erica Norris <norrise120@gmail.com>
2019-03-01 12:30:37 -0800

    Updated fulfillment_status variable to pass tests.  Added accessor for instance variables (except id)

--- lib/order.rb
+++ lib/order.rb
@@ -1,11 +1,17 @@
 class Order
   attr_reader :id
+  attr_accessor :products, :customer, :fulfillment_status
 
-  def initialize(id, products, customer, fulfillment_status: :pending)
+  @@valid_statuses = %i[pending paid processing shipped complete]
+
+  def initialize(id, products, customer, fulfillment_status = :pending)
     @id = id
     @products = products
     @customer = customer
     @fulfillment_status = fulfillment_status
+    unless @@valid_statuses.include?(@fulfillment_status)
+      raise ArgumentError, "You must provide valid fulfillment status."
+    end
   end
 
   def total
