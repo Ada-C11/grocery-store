2738e3e2ed05996fe14c266de812d5edccebff44
Kate Sandler <kate@katekatekate.net>
2019-02-28 21:12:14 -0800

    update total method to add tax

--- lib/order.rb
+++ lib/order.rb
@@ -2,21 +2,22 @@ class Order
   attr_accessor :products, :customer, :fulfillment_status
   attr_reader :id
 
-  def initialize(id, products, customer, fulfillment_status)
+  def initialize(id, products, customer, fulfillment_status = :pending)
     @id = id
     @products = products
     @customer = customer
     @fulfillment_status = fulfillment_status
     fs_types = %i[pending paid processing shipped complete]
-    raise ArgumentError, 'Wrong status' if fs_types.exclude? @fulfillment_status
+    raise ArgumentError, 'Wrong status' if fs_types.none? @fulfillment_status
   end
-  
+
   def total(products)
     (products.values.sum * 1.075).round(2)
   end
-  
+
   def add_product(name, price)
-    # add the data to the product collection
-    # raise ArgumentError if there is already a product with the same name
+    raise ArgumentError if product.key?(name)
+
+    products[name] = price
   end
 end
