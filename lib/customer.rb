c9b8a33e2b3e4113fb911f4579594e7d7c25c7d3
Angela <angela.ohh@gmail.com>
2019-02-28 16:34:32 -0800

    created self.all class method

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,4 +1,4 @@
-
+require "csv"
 
 class Customer
   attr_reader :id
@@ -9,4 +9,20 @@ class Customer
     @email = email
     @address = address
   end
+
+  def self.all
+    all_customer_info = []
+    CSV.open("/Users/angelaoh/documents/grocery-store/data/customers.csv", "r").each do |info|
+      # change ID into integer, not string
+      info[0] = info[0].to_i
+
+      customer_info_instance = Customer.new(info[0], info[1], info[2])
+      all_customer_info << customer_info_instance
+    end
+
+    return all_customer_info
+  end
+
+  #   def find(id)
+  #   end
 end
