80e8ca0e9687c7ec3c2d4686959c63bdc7ad3e6f
Grace Shea <grace.m.shea@gmail.com>
2019-03-01 17:10:59 -0800

    class method to read customer csv

--- lib/customer.rb
+++ lib/customer.rb
@@ -7,4 +7,24 @@ class Customer
     @email = email
     @address = address
   end
+
+  def self.all
+    @customers = []
+    CSV.open("./data/customers.csv", "r").each do |row|
+      id = row[0].to_i
+      email = row[1].to_s
+      address = {
+        street: row[2].to_s,
+        city: row[3].to_s,
+        state: row[4].to_s,
+        zip: row[5].to_s,
+      }
+      customer = Customer.new(id, email, address)
+      @customers << customer
+    end
+    return @customers
+  end
+
+  def self.find(id)
+  end
 end
