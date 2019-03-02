d60fbe791a40a3c85184e074ea0b38de60de5d8c
Mina Shin <minamishin@hotmail.com>
2019-03-02 13:40:23 -0800

    pushing changes

--- lib/order.rb
+++ lib/order.rb
@@ -1,17 +1,20 @@
 require 'csv'
 require 'awesome_print'
+require_relative 'customer'
 
 class Order
-    attr_reader :id, :products, :customer, :fulfillment_status
+    attr_reader :id
+    attr_accessor :products, :customer, :fulfillment_status
 
     def initialize(id, products, customer, fulfillment_status = :pending)
         @id = id
         @products = products
         @customer = customer
         @fulfillment_status = fulfillment_status
-        
-        if @fulfillment_status != :paid && @fulfillment_status != :processing && @fulfillment_status != :shipped && @fulfillment_status != :complete && @fulfillment_status != :pending
-            raise ArgumentError, "No fulfillment status given"
+
+        status = [:paid, :processing, :shippped, :complete, :pending]
+        if !status.include?(fulfillment_status)
+            raise ArgumentError, "No valid fulfillment status given."
         end
     end
 
@@ -34,11 +37,22 @@ class Order
         @products[product_name] = [price]
     end
 
-    # open CSV and put into an array each line of the CSV
+    # open CSV and put into an array each line of the CSV; creating each parameter of Order.new and putting products into hash
     def self.all
         new_order = []
         CSV.open('data/orders.csv', 'r').each do |line|
-            new_order << Order.new(line[0], line[1], line[2], line[3])
+            product_hash = {}
+            id = line[0].to_i
+            products = line[1].split(';')
+            products.each do |each_item|
+                product = each_item.split(':')
+                product_hash[product[0]] = product[1].to_f
+            end
+            products = product_hash
+            customer = Customer.find(line[2].to_i)
+            fulfillment_status = line[3].to_sym
+            orders = Order.new(id, products, customer, fulfillment_status)
+            new_order << orders
         end
         return new_order
     end
