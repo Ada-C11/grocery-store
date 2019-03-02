b6057e73b9a16abb52ea7c46855c654cd2dc61d4
Jessica <jessica.homet@gmail.com>
2019-03-01 21:55:39 -0800

    refactored to split long lines of code onto multiple lines

--- lib/order.rb
+++ lib/order.rb
@@ -1,14 +1,22 @@
 class Order
   attr_reader :id, :products, :customer, :fulfillment_status
+
   @@tax_rate = 0.075
 
-  def initialize(id, products, customer, fulfillment_status = :pending)
+  def initialize(id,
+                 products,
+                 customer,
+                 fulfillment_status = :pending)
     @id = id
     @products = products
     @customer = customer
     @fulfillment_status = fulfillment_status
 
-    unless fulfillment_status == :pending || fulfillment_status == :paid || fulfillment_status == :processing || fulfillment_status == :shipped || fulfillment_status == :complete
+    unless fulfillment_status == :pending ||
+           fulfillment_status == :paid ||
+           fulfillment_status == :processing ||
+           fulfillment_status == :shipped ||
+           fulfillment_status == :complete
       raise ArgumentError, "invalid fulfillment status"
     end
   end
@@ -22,7 +30,10 @@ class Order
         pair = i.split(":")
         hash[pair[0]] = pair[1].to_f
       end
-      orders_array << Order.new(line[0].to_i, hash, Customer.find(line[2].to_i), line[3].to_sym)
+      orders_array << Order.new(line[0].to_i,
+                                hash,
+                                Customer.find(line[2].to_i),
+                                line[3].to_sym)
     end
 
     return orders_array
@@ -56,7 +67,8 @@ class Order
     if @products.keys.include?(product_name)
       @products.delete(product_name)
     else
-      raise ArgumentError, "you cannot remove a product that does not exist"
+      raise ArgumentError, "you cannot remove a product that" \
+            "does not exist"
     end
   end
 end
