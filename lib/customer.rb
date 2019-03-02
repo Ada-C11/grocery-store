3fcff069b49167c99428cf3f7c903aa25966c481
Amy Wyatt <amy.wyatt12@outlook.com>
2019-03-01 22:45:39 -0800

    Added abilities to list all customers and ability to find specific customer

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,6 @@
+require "csv"
+require "awesome_print"
+
 class Customer
   attr_reader :id
   attr_accessor :email, :address
@@ -12,5 +15,30 @@ class Customer
   def customer_info
     customer = { id: @id, email: @email, address: @address }
     return customer
+  end
+
+  # collects all customers from csv file
+  def self.all
+    customers = []
+
+    CSV.open("data/customers.csv", "r").each do |customer_info|
+      customer_address = { street: customer_info[2],
+                          city: customer_info[3],
+                          state: customer_info[4],
+                          zip: customer_info[5] }
+      new_customer = Customer.new(customer_info[0].to_i, customer_info[1], customer_address)
+      customers << new_customer
+    end
+    return customers
+  end
+
+  # finds specfic customer
+  def self.find(id)
+    Customer.all.each do |customer|
+      if customer.id == id
+        return customer
+      end
+    end
+    return nil
   end
 end
