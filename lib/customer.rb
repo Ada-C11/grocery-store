2b32ed9e2969b72421c3395332bda24ec375ca86
Mello-Cello <celloelle@gmail.com>
2019-03-03 14:48:07 -0800

    Wave 2 complete. Optional item at end of wave two unfinished and commented out.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,7 @@
+require "csv"
+require "awesome_print"
+# require_relative '../data/customers.csv'
+
 class Customer
   attr_accessor :email, :address
   attr_reader :id
@@ -6,5 +10,24 @@ class Customer
     @id = id
     @email = email
     @address = address
+  end
+
+  def self.all
+    customers = [] # this is an array of instances of the Customer
+
+    CSV.open("data/customers.csv", "r").each do |customer_row|
+      customers << Customer.new(customer_row[0].to_i, customer_row[1], {street: customer_row[2], city: customer_row[3], state: customer_row[4], zipcode: customer_row[5]})
+    end
+
+    return customers
+  end
+
+  def self.find(id)
+    self.all.each do |customer|
+      if customer.id == id
+        return customer # an instance of Customer where the value of the id field in the CSV matches the passed parameter
+      end
+    end
+    return nil # <-- Exit the loop. If the id wasn't found by the if statement, return nil.
   end
 end
--- lib/order.rb
+++ lib/order.rb
@@ -20,24 +20,13 @@ class Order
   end
 
   def total
-    # A total method which will calculate the total cost of the products by:
-    # Summing up the products
-    # Adding a 7.5% tax
-    # Rounding the result to two decimal places
-
-    # A collection of products and their cost will be given as a hash that looks like this:
-    # { "banana" => 1.99, "cracker" => 3.00 }
-    # Zero products is permitted (an empty hash)
-    # You can assume that there is only one of each product
-
+    # calculate the total cost of the products incl 7.5% tax; round to two decimal places
     order_total = (products.values.sum * 1.075).truncate(2)
-
     return order_total
   end
 
   def add_product(product_name, product_price)
-    # An add_product method which will take in two parameters, product name and price, and add the data to the product collection
-    # If a product with the same name has already been added to the products, an ArgumentError should be raised
+    # add a new product to the product collection; if a product with the same name has already been added to the products, an ArgumentError should be raised
     if products.keys.include?(product_name)
       raise ArgumentError, "This product has already been ordered."
     end
@@ -46,12 +35,64 @@ class Order
   end
 
   def remove_product(product_name)
-    # Add a remove_product method to the Order class which will take in one parameter, a product name, and remove the product from the collection
-    # If no product with that name was found, an ArgumentError should be raised
+    # WRITING THIS METHOD WAS OPTIONAL
+    # remove a specified product from the collection; if no product with that name was found, an ArgumentError should be raised
     if products.keys.include?(product_name)
       products.delete(product_name)
     else
       raise ArgumentError, "Product not found."
     end
   end
+
+  def self.products_helper(products_from_csv_row) # <-- convert string to hash
+    # "Capers:20.66;Tarragon:36.04;Arborio rice:67.06;Bean Shoots:41.97"
+    products_hash = {}
+    products_array_1 = products_from_csv_row.split(";")
+    # products_array_1 = ["Capers:20.66", "Tarragon:36.04", "Arborio rice:67.06", "Bean Shoots:41.97"]
+    products_array_1.each do |product_and_price_string|
+      products_array_2 = product_and_price_string.split(":")
+      # product_and_price_stirng ["Capers", "20.66"]
+      products_hash[products_array_2[0]] = products_array_2[1].to_f
+    end
+
+    return products_hash
+  end
+
+  def self.all
+    orders = [] # this is an array of instances of Order
+
+    CSV.open("data/orders.csv", "r").each do |order_row| # <-- order_row should be an array of strings
+      # example: ["31","Capers:20.66;Tarragon:36.04;Arborio rice:67.06;Bean Shoots:41.97","13","processing"]
+      # parameters to pass to Order.new: order id, products, cust id, fulfill status
+      orders << Order.new(order_row[0].to_i, self.products_helper(order_row[1]), Customer.find(order_row[2].to_i), order_row[3].to_sym)
+    end
+
+    return orders
+  end
+
+  def self.find(id)
+    self.all.each do |order|
+      if order.id == id
+        return order # an instance of Order where the value of the id field in the CSV matches the passed paramater
+      end
+    end
+    return nil # <-- Exit the loop. If the id wasn't found by the if statement, return nil.
+  end
+
+  # def Order.find_by_customer(customer_id)
+  #   # WRITING THIS METHOD IS OPTIONAL
+  #   orders_by_this_customer = []
+  #   # this_customer_id = customer_id
+  #   orders_to_check = self.all
+  #   orders_to_check.each do |order_instance|
+  #     puts "order_instance.customer"
+  #     puts order_instance.customer
+  #     if order_instance.customer == customer_id
+  #       orders_by_this_customer << order_instance
+  #     end
+  #     return orders_by_this_customer # a list [] of Order instances where the value of the customer's ID matches the passed parameter
+  #   end
+  # end
 end
+
+# Order.find_by_customer(25)
