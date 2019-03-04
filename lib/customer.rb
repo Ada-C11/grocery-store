dd865ab8e927fd72a81d24648342c18afab4b6dd
Erica Norris <norrise120@gmail.com>
2019-03-03 19:02:39 -0800

    Added self.all method to Customer class

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 class Customer
   attr_reader :id
   attr_accessor :email, :address
@@ -7,4 +9,15 @@ class Customer
     @email = email
     @address = address
   end
+
+  def self.all
+    customers = []
+    CSV.read("data/customers.csv").each do |row|
+      customer_address = {street: row[2], city: row[3], state: row[4], zip: row[5]}
+      customers.push(Customer.new(row[0].to_i, row[1], customer_address))
+    end
+    return customers
+  end
 end
+
+Customer.all
