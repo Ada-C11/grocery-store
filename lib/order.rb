ba21ee7c6e8a9250e02a5517f0fbba7445d24098
Amy Wyatt <amy.wyatt12@outlook.com>
2019-03-04 13:05:07 -0800

    Added find customer functionality

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 class Order
   attr_reader :id, :products, :customer
   attr_accessor :fulfillment_status
@@ -38,5 +40,33 @@ class Order
     raise ArgumentError if !@products.keys.include?(product_name)
     @products.delete(product_name)
     return @products
+  end
+
+  # collects all orders
+  def self.all
+    orders = []
+
+    CSV.open("data/orders.csv", "r").each do |order_info|
+      products_array = order_info[1].split(/[;:]/)
+      products_hash = Hash[*products_array]
+      products_hash.each { |product, price| products_hash[product] = price.to_f }
+
+      new_order = Order.new(order_info[0].to_i,
+                            products_hash,
+                            Customer.find(order_info[2].to_i),
+                            order_info[3].to_sym)
+      orders << new_order
+    end
+    return orders
+  end
+
+  # finds specific order within collection
+  def self.find(id)
+    Order.all.each do |order|
+      if order.id == id
+        return order
+      end
+    end
+    return nil
   end
 end
