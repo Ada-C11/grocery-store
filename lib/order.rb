3451dfb2cee1bfe3eb89480d2f58b89d81788124
Maria Wissler <wmcarmelina@gmail.com>
2019-03-03 20:56:08 -0800

    identation format

--- lib/order.rb
+++ lib/order.rb
@@ -1,10 +1,10 @@
 # Orders
-require_relative "customer.rb"
+require_relative 'customer.rb'
 require 'csv'
 
 class Order
-    attr_reader :id
-    attr_accessor :products, :customer, :fulfillment_status
+  attr_reader :id
+  attr_accessor :products, :customer, :fulfillment_status
 
   def initialize(id, products, customer, fulfillment_status = :pending)
     @id = id
@@ -12,58 +12,53 @@ class Order
     @customer = customer
     @fulfillment_status = fulfillment_status
 
-     valid_statuses = %i[pending paid processing shipped complete]
-      unless valid_statuses.include?(@fulfillment_status)
-      raise ArgumentError
-      end 
-    end 
+    valid_statuses = %i[pending paid processing shipped complete]
+    raise ArgumentError unless valid_statuses.include?(@fulfillment_status)
+    end
 
-
-    def total
-    products_total = @products.map {|item, price| price}.sum
+  def total
+    products_total = @products.map { |_item, price| price }.sum
     tax = products_total * 0.075
     total = (products_total + tax).round(2)
-    return total 
-    end
-    
-    def add_product(product_name, price)
-        if @products.include? (product_name)
-          raise ArgumentError
-         else 
-          @products.store(product_name, price)
-        end 
-    end
+    total
+  end
 
-    def self.all 
-        order_data = []
-        CSV.open("../data/orders.csv", "r").each do |line|
-            id = line[0].to_i
-            split_product= line[1].split(';')
-            products_purchased = {}
-            split_product.each do |item|
-                product = item.split(':')
-                products_purchased[product[0]] = product[1].to_f 
-            end
-            products_list = products_purchased
-            customer_id = line[2].to_i
-            customer = Customer.find(customer_id)
-            status = line[3].to_sym
-            orders = Order.new(id, products_list, customer, status)
-            order_data << orders
-        end
-        return order_data
+  def add_product(product_name, price)
+    if @products.include? product_name
+      raise ArgumentError
+    else
+      @products.store(product_name, price)
     end
-        
+  end
 
-        def self.find(id)
-            order_match = nil 
-            self.all.each do |current_order|
-              if current_order.id == id 
-                order_match = current_order
-                break
-              end 
-        end 
-            return order_match
+  def self.all
+    order_data = []
+    CSV.open('../data/orders.csv', 'r').each do |line|
+      id = line[0].to_i
+      split_product = line[1].split(';')
+      products_purchased = {}
+      split_product.each do |item|
+        product = item.split(':')
+        products_purchased[product[0]] = product[1].to_f
+      end
+      products_list = products_purchased
+      customer_id = line[2].to_i
+      customer = Customer.find(customer_id)
+      status = line[3].to_sym
+      orders = Order.new(id, products_list, customer, status)
+      order_data << orders
     end
+    order_data
+  end
 
+  def self.find(id)
+    order_match = nil
+    all.each do |current_order|
+      if current_order.id == id
+        order_match = current_order
+        break
+      end
+    end
+    order_match
+end
 end
