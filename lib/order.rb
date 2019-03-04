a24656f9320d9c5c3a29bce0e6712afb222fa2e8
Erica Norris <norrise120@gmail.com>
2019-03-03 21:43:39 -0800

    Added all class method to Order class

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 class Order
   attr_reader :id
   attr_accessor :products, :customer, :fulfillment_status
@@ -26,5 +28,19 @@ class Order
     else
       @products[name] = price
     end
+  end
+
+  def self.all
+    orders = []
+    CSV.read("data/orders.csv").each do |row|
+      product_list = {}
+      customer_products = row[1].split(";")
+      customer_products.each do |string|
+        product_and_price = string.split(":")
+        product_list[product_and_price[0]] = product_and_price[1].to_f
+      end
+      orders.push(Order.new(row[0].to_i, product_list, Customer.find(row[2].to_i), row[3].to_sym))
+    end
+    return orders
   end
 end
