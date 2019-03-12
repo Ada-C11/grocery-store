8e4e80123a5ff6866ad372086eb15c972ae8df1e
Chantal Demissie <chantaldemissie@yahoo.com>
2019-03-11 22:59:12 -0700

    Add Wave 2 solution and tests passing.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 class Customer
   attr_reader :id
   attr_accessor :email, :address
@@ -7,4 +9,20 @@ class Customer
     @email = email
     @address = address
   end
+
+  def self.all
+    customers = []
+    headers = ['id', 'email', 'address', 'city', 'state', 'zip code']
+    CSV.foreach("data/customers.csv", :headers => headers).each do |row|
+      address = "#{row['address']}, #{row['city']}, #{row['state']}, #{row['zip code']}"
+      customer = Customer.new(row['id'].to_i, row['email'], address)
+      customers << customer
+    end
+    return customers
+  end
+
+  def self.find(id)
+    self.all.find{|customer| id == customer.id}
+  end
+
 end
--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,7 @@
+require 'csv'
+
+require_relative 'customer'
+
 class Order
   attr_reader :id, :products, :customer, :fulfillment_status
 
@@ -26,4 +30,24 @@ class Order
     end
     @products[product_name] = price
   end
+
+  def self.all
+    orders = []
+    headers = ['id', 'products', 'customer id', 'status']
+    CSV.foreach("data/orders.csv", :headers => headers).each do |row|
+      products = row['products'].split(";").map{ |key_value_pair|
+        pair = key_value_pair.split(":")
+        [pair[0], pair[1].to_f]
+      }.to_h
+      customer = Customer.find(row['customer id'].to_i)
+      order = Order.new(row['id'].to_i, products, customer, row['status'].to_sym)
+      orders << order
+    end
+    return orders
+  end
+
+  def self.find(id)
+    self.all.find{|order| id == order.id}
+  end
+
 end
